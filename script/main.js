79844695da46cb6bf68c1fa1b57a5c304cf73236
Anthony Sanchez <ar9829415@gmail.com>
2023-01-07 23:10:57 -0600

    Home, category

--- script/main.js
+++ script/main.js
@@ -1,14 +1,57 @@
 // Variables iniciales
 let shoppingCartArray = []; //Los productos que se vayan sumando
-let CategoryArray = [];
-let listBooks = [];
-let total = 0;
-let active = true;
-const loginSection = document.getElementById('login-section');
-const registerSection = document.getElementById('register-section');
-const containerGallery = document.getElementById('container-gallery')
-const carrusel_inner = document.getElementById('carousel-inner')
-var body  = document.body;
+let CategoryArray     = [];
+let listBooks         = [];
+let total             = 0;
+
+const loginSection     = document.getElementById('login-section');
+const registerSection  = document.getElementById('register-section');
+const containerGallery = document.getElementById('container-gallery');
+const checkoutSection  = document.getElementById('checkout-section');
+const carrusel_inner   = document.getElementById('carousel-inner');
+const headerBooks      = document.getElementById('header-books');
+const navRight         = document.getElementById('nav-books-right');
+const navLeft          = document.getElementById('nav-books-left');
+let cartContainer      = document.querySelector('.cart-items');
+let totalElement       = document.getElementById('cart-total-title');
+const subMenuGrap      = document.getElementById('subMenuGrap');
+const iconUser         = document.getElementById('icon-user');
+const subMenuGrapUser  = document.getElementById('subMenuGrapUser');
+const btnCategory      = document.getElementById('btn-category');
+
+var body               = document.body;
+var html               = document.getElementById('html')
+
+
+window.addEventListener('scroll', function() {
+	if ( window.pageYOffset > 733 ){
+		headerBooks.classList.add('change');
+		navLeft.classList.add('changeL')
+		navRight.classList.add('changeR')
+	}
+	else{
+		headerBooks.classList.remove('change')
+		navLeft.classList.remove('changeL')
+		navRight.classList.remove('changeR')
+	}
+});
+btnCategory.addEventListener( 'mouseover', () =>{
+	subMenuGrap.classList.add('Open_option');
+	headerBooks.classList.add('header-max');
+} )
+btnCategory.addEventListener( 'mouseout', () =>{
+	subMenuGrap.classList.remove('Open_option');
+	headerBooks.classList.remove('header-max');
+} )
+
+iconUser.addEventListener( 'mouseover', () =>{
+	subMenuGrapUser.classList.add('Open_option');
+	headerBooks.classList.add('header-max');
+} )
+iconUser.addEventListener( 'mouseout', () =>{
+	subMenuGrapUser.classList.remove('Open_option');
+	headerBooks.classList.remove('header-max');
+} )
 
 function scrollNavigation( section ){
 	var scroll = document.getElementById(section);
@@ -23,10 +66,16 @@ function on_modal( on_section, off_section ) {
 	Off_section.classList.remove('active_modal');
 
 	body.classList.add('active_overflow');
+	html.classList.add('active_overflow');
+}
+function modal(){
+	checkoutSection.classList.add('active_modal');
 }
 function close_modal() {
 	loginSection.classList.remove('active_modal');
 	registerSection.classList.remove('active_modal');
+	checkoutSection.classList.remove('active_modal');
 
 	body.classList.remove('active_overflow');
+	html.classList.remove('active_overflow');
 }
--- script/script.js
+++ script/script.js
@@ -14,15 +14,15 @@ const productsArray = data.results
 
 console.log(productsArray)
 
-
+// ? categories[listBooks[0].categories.length - 1]
 productsArray.forEach( book => {
     listBooks.push( book )
 
     if ( listBooks.length === 3 ){
         carrusel_inner.innerHTML +=`
-        <div class="carousel-item ">
+        <div class="carousel-item">
             <div class="row">
-                <div class="col-md-4" id="${ listBooks[0].work_id }">
+                <div class="col-md-4">
                     <div class="">
                         <div class="item-box-blog-image">
                             <!--Image-->
@@ -33,24 +33,24 @@ productsArray.forEach( book => {
                             <div class="item-box-blog-heading">
                                 <div>
                                     <a class="text-tittle" href="#" tabindex="0">
-                                        <h5>${ listBooks[0].categories[0] }</h5>
+                                        <h5>${ listBooks[0].title }</h5>
                                     </a>
                                 </div>
                                 <!--Data-->
                                 <div class="item-box-blog-data" style="padding: px 15px;">
-                                    <p class="text-price"><i class="fa fa-user-o"></i>$ ${ listBooks[0].page_count }.00</p>
+                                    <p class="text-price">$ ${ listBooks[0].page_count }.00</p>
                                 </div>
                             </div>
                             <div class="mt">
-                                <a href="#" tabindex="0" class="white read">
-                                    <img src="../assets/icon/cart.svg" alt="">
-                                </a>
+                                <div tabindex="0" class="white read">
+                                    <img class="shop-item-button"  id="${ listBooks[0].work_id }" src="../assets/icon/cart.svg" alt="">
+                                </div>
                             </div>
                         <!--Read More Button-->
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4" id="${ listBooks[1].work_id }">
+                <div class="col-md-4">
                     <div class="">
                         <div class="item-box-blog-image">
                             <!--Image-->
@@ -61,25 +61,25 @@ productsArray.forEach( book => {
                             <div>
                                 <div class="item-box-blog-heading">
                                     <a class="text-tittle" href="#" tabindex="0">
-                                        <h5>${ listBooks[1].categories[0] }</h5>
+                                        <h5>${ listBooks[1].title }</h5>
                                     </a>
                                 </div>
                                 <!--Data-->
                                 <div class="item-box-blog-data" style="padding: px 15px;">
-                                    <p class="text-price"><i class="fa fa-user-o"></i>$ ${ listBooks[1].page_count }.00</p>
+                                    <p class="text-price">$ ${ listBooks[1].page_count }.00</p>
                                 </div>
                                 <!--Text-->
                             </div>
                             <div class="mt">
-                                <a href="#" tabindex="0" class="  white read">
-                                    <img src="../assets/icon/cart.svg" alt="">
-                                </a>
+                                <div tabindex="0" class="white read">
+                                    <img class="shop-item-button" id="${ listBooks[1].work_id }" src="../assets/icon/cart.svg" alt="">
+                                </div>
                             </div>
                             <!--Read More Button-->
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4" id="${ listBooks[2].work_id }">
+                <div class="col-md-4">
                     <div class="">
                         <div class="item-box-blog-image">
                             <!--Image-->
@@ -90,20 +90,20 @@ productsArray.forEach( book => {
                             <div>
                                 <div class="item-box-blog-heading">
                                     <a class="text-tittle" href="#" tabindex="0">
-                                        <h5>${ listBooks[2].categories[0] }</h5>
+                                        <h5>${ listBooks[2].title }</h5>
                                     </a>
                                 </div>
                                 <!--Data-->
                                 <div class="item-box-blog-data" style="padding: px 15px;">
                                     <p class="text-price">
-                                        <i class="fa fa-user-o"></i>$ ${ listBooks[2].page_count }.00
+                                        $ ${ listBooks[2].page_count }.00
                                     </p>
                                 </div>
                             </div>
                             <div class="mt">
-                                <a href="#" tabindex="0" class=" white read">
-                                    <img src="../assets/icon/cart.svg" alt="">
-                                </a>
+                                <div tabindex="0" class=" white read">
+                                    <img id="${ listBooks[2].work_id }" class="shop-item-button" src="../assets/icon/cart.svg" alt="">
+                                </div>
                             </div>
                         <!--Read More Button-->
                         </div>
@@ -118,10 +118,145 @@ productsArray.forEach( book => {
         listBooks.pop();
         listBooks.pop();
     }
-    
-    // element.categories.forEach( category => {
-    //     if ( CategoryArray.includes( category ) == false ){
-    //         CategoryArray.push( category )
-    //     }
-    // } )
 });
+
+let carouselItem = document.querySelectorAll('.carousel-item');
+let addBtns      = document.querySelectorAll('.shop-item-button');
+let shopButton   = document.querySelectorAll('.shop-item-button');
+
+carouselItem[0].classList.add('active')
+addBtns = [...addBtns];
+shopButton = [...shopButton]
+shopButton.forEach( item => {
+	item.addEventListener( 'mouseover', img => {
+		item.src = "../assets/icon/cartWhite.svg"
+	} )
+	item.addEventListener( 'mouseout', img => {
+		item.src = "../assets/icon/cart.svg"
+	} )
+} )
+
+addBtns.forEach(  btn => {
+    btn.addEventListener( 'click', e =>{
+        let actualID = parseInt(e.target.id);
+
+        // Con el ID encontrar el objeto actual
+        let actualProduct = productsArray.find( item => item.work_id == actualID );
+
+        if ( actualProduct.quantity === undefined ){
+            actualProduct.quantity = 1; // ? Modifico una propiedad
+        }
+
+        let validar = false;
+        // Pregutar si el producto es repetido
+        shoppingCartArray.forEach( libros => {
+            if ( libros.work_id == actualID ){
+                validar = true;
+            }
+        } );
+
+        if ( validar ){
+            actualProduct.quantity ++;
+        }
+        else{
+            console.log(actualProduct.quantity)
+            shoppingCartArray.push(actualProduct);
+        }
+
+        // Dibujar en el DOM el arreglo actualizado
+        drowItems();
+
+        // Actualizar el valor
+        getTotal();
+
+        // Actualizar el numero de items
+        updateNumberOfItems();
+
+        removeItems();
+    } )
+})
+
+function getTotal(){
+    let sumTotal;
+    // ? reduce -> agrupa los mismos elementos dentro de un arreglo
+    let total = shoppingCartArray.reduce( (sum, item )=>{ // * item: representa cada uno de los objetos de cart
+        sumTotal = sum + item.quantity*item.page_count;
+        return sumTotal;
+    } ,0 );
+
+    totalElement.innerHTML = `Total: $${total}`;
+};
+
+function drowItems(){
+    cartContainer.innerHTML = '';
+    shoppingCartArray.forEach( item => {
+        cartContainer.innerHTML += `
+        <div class="cart-row">
+            <div class="cart-item cart-column">
+                <img class="cart-item-image" src="${ item.published_works[0].cover_art_url }" width="100" height="100">
+                <div class="cart-item-ctn"><span class="cart-item-title">${ item.title }</span></div>
+            </div>
+            <span class="cart-price cart-column">$ ${ item.page_count }. 00</span>
+            <div class="cart-quantity cart-column">
+                <input class="cart-quantity-input" min="1" type="number" value="${ item.quantity }">
+                <img class="remove-item" src="../assets/icon/close.svg" alt="">
+            </div>
+        </div>`;
+    } );
+
+    removeItems();
+}
+
+
+function updateNumberOfItems(){
+    let quantityValue = document.querySelectorAll('.cart-quantity-input');
+    quantityValue = [...quantityValue];
+
+    quantityValue.forEach( value =>{
+        value.addEventListener( 'click', event =>{
+            console.log( event )
+            // conseguir el titulo del libro
+            let actualBooTitle = event.target.parentElement.parentElement.childNodes[1].innerText;
+            let actualBookQuantity  = parseInt(event.target.value);
+
+            console.log(actualBooTitle)
+            // Busco el objeto atraves del titulo
+            let actualBookObjet = shoppingCartArray.find( item => item.title == actualBooTitle ); // ? Buscar el libro que selecciono
+
+            // Actualizar el numero de quantity
+            actualBookObjet.quantity = actualBookQuantity;
+
+            // Actualizar el precio
+            getTotal();
+        } );
+    } );
+};
+
+
+function removeItems(){
+    let removeBtn = document.querySelectorAll('.remove-item');
+    removeBtn = [...removeBtn];
+
+    removeBtn.forEach( btn => {
+        btn.addEventListener( 'click', event =>{
+            // conseguir el titulo del libro
+            let actualBooTitle = event.target.parentElement.parentElement.childNodes[1].innerText;
+
+            // Busco el objeto atraves del titulo
+            let actualBookObjet = shoppingCartArray.find( item => item.title == actualBooTitle ); // ? Buscar el libro que selecciono
+
+            // Remover el arreglo de producto de cart
+            shoppingCartArray = shoppingCartArray.filter( item => item != actualBookObjet );
+
+            // Dibujar en el DOM el arreglo actualizado
+            drowItems();
+
+            // Actualizar el valor
+            getTotal();
+
+            // Actualizar el numero de items
+            updateNumberOfItems();
+
+        } );
+    } );
+};
